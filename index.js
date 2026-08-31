f730cf63c80a8ccd9bde70b9b37bc17f537d7822
Amr Ahmed <amrahmed09951@gmail.com>
2026-09-01 02:17:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const textBox = document.getElementById("textBox");
+const toFah = document.getElementById("toFah");
+const toCel = document.getElementById("toCel");
+const result = document.getElementById("result");
+let temp;
+function convert(){
+    if(toFah.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp + " F";
+    }
+    else if(toCel.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp + " C";
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
+}
